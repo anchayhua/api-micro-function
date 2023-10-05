c3e1c12f0bc2d3530967a918883e88ced587933d
Quique <ncnr_quique@hotmail.com>
2023-10-05 16:03:58 -0500

    test

--- src/test/java/com/quique/apifuntion/ApiFuntionApplicationTests.java
+++ src/test/java/com/quique/apifuntion/ApiFuntionApplicationTests.java
@@ -1,22 +1,11 @@
 package com.quique.apifuntion;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
-
-import java.nio.charset.StandardCharsets;
-import java.time.Duration;
 
 @SpringBootTest
 @AutoConfigureWebTestClient
